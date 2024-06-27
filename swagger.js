40a1b71bed6ea30fa55b3be792349d79d912da4b
MahdiGts <kattoussimahdi1@gmail.com>
2024-06-27 16:17:32 +0100

    Add PORT

--- swagger.js
+++ swagger.js
@@ -3,9 +3,10 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
+const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 8000');
 });
